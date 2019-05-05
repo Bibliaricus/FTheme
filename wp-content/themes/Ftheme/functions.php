4b1242635c014f2f49fdc7c61ffee78d9e8526ae
Bibliaricus <slavikdev95@gmail.com>
2019-05-05 23:30:35 +0300

    Adds main files for themes

---
+++ wp-content/themes/Ftheme/functions.php
@@ -0,0 +1,29 @@
+<?php
+/*
+functions.php
+
+The theme's functions and definitions.
+*/
+
+/***********************************************************
+1.0 - Define constants.
+***********************************************************/
+
+define( 'THEMEROOT', get_stylesheet_directory_uri() );
+define( 'IMAGES', THEMEROOT . '/images');
+define( 'SCRIPTS', THEMEROOT . '/js');
+define( 'FRAMEWORK', get_template_directory() . /framework);
+
+/*****************************
+ * 
+ * 
+ * 
+ */
+
+/**
+ * ----------------------------------------------------
+ * 2.0 - Load the framework.
+ * -------------------------------------------
+ */
+require_once( FRAMEWORK . '/init.php');
+?>
