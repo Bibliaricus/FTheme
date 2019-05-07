8f66e273b83053b6310fe8829f787be49f97f7e9
Bibliaricus <slavikdev95@gmail.com>
2019-05-07 10:59:08 +0300

    Edit function.php and index.php

--- wp-content/themes/Ftheme/functions.php
+++ wp-content/themes/Ftheme/functions.php
@@ -12,13 +12,7 @@ The theme's functions and definitions.
 define( 'THEMEROOT', get_stylesheet_directory_uri() );
 define( 'IMAGES', THEMEROOT . '/images');
 define( 'SCRIPTS', THEMEROOT . '/js');
-define( 'FRAMEWORK', get_template_directory() . /framework);
-
-/*****************************
- * 
- * 
- * 
- */
+define( 'FRAMEWORK', get_template_directory() . '/framework');
 
 /**
  * ----------------------------------------------------
@@ -26,4 +20,64 @@ define( 'FRAMEWORK', get_template_directory() . /framework);
  * -------------------------------------------
  */
 require_once( FRAMEWORK . '/init.php');
+
+/**
+ * -------------------------------------------------------
+ * 3.0 - Set up the content value based on theme's design.
+ * -------------------------------------------------------
+ */
+if ( !isset( $content_width ) ) {
+  $content_width = 800;
+}
+
+/**
+ * -------------------------------------------------------
+ * 4.0 - Set up theme default and register various supported features.
+ * -------------------------------------------------------
+ */
+if ( ! function_exists( 'ftheme_setup' ) ) {
+  function ftheme_setup() {
+    /**
+     * Make the theme available for translation.
+     */
+    $lang_dir = THEMEROOT . '/languages';
+    load_theme_textdomain( 'ftheme', $lang_dir );
+  }
+
+  /**
+   * Add support for post formats.
+   */
+  add_theme_support( 'post-formats',
+    array(
+      'gallery',
+      'link',
+      'image',
+      'quote',
+      'video',
+      'audio'
+    )
+  );
+
+  /**
+   * Add support for automatic feed links.
+   */
+  add_theme_support( 'automatic-feed-links' );
+
+  /**
+   * Add support for post thumbnails.
+   */
+  add_theme_support( 'post-thumbnails' );
+
+  /**
+   * Register nav menus.
+   */
+  register_nav_menus(
+    array(
+      'main-menu' => __( 'Main Menu', 'ftheme' )
+    )
+  );
+  
+  add_action( 'after_theme_setup', 'alpha_setup' );
+}
+
 ?>
