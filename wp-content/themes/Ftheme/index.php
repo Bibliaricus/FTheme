3bfa9f64508cd5ffe08cf7909790a29afe6699a3
Bibliaricus <slavikdev95@gmail.com>
2019-05-07 12:32:30 +0300

    Add post header (thumbnail, tilte (in dependence on single page or not) as link)

--- wp-content/themes/Ftheme/index.php
+++ wp-content/themes/Ftheme/index.php
@@ -7,7 +7,7 @@
 ?>
 
 <div class="main-content col-md-8" role="main">
-  <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php get_template_part( 'content', get_post_format() ); ?>
   <?php endwhile; ?>
 
