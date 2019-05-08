0bba6f4a5a7dc8ac29d45a578d0fd494f3c89bc5
Bibliaricus <slavikdev95@gmail.com>
2019-05-08 10:36:51 +0300

    Add meta-information to post (author, date, category, tags). Add comment edit link

--- wp-content/themes/Ftheme/functions.php
+++ wp-content/themes/Ftheme/functions.php
@@ -92,6 +92,42 @@ if ( ! function_exists( 'ftheme_post_meta' ) ) {
       if ( is_sticky() ) {
         echo '<li class="meta-featured-post"><i class="fa fa-thumb-tack"></i>' . __( 'Sticky', 'ftheme' ) . '</li>';
       }
+
+      // Get the post author.
+      printf(
+        '<li class="meta-author"><a href="%1s" rel="author">%2s</a></li>',
+        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+        get_the_author()
+      );
+
+      // Get the date.
+      echo '<li class="meta-data"> ' . get_the_date() . ' </li>';
+
+      // Get the categories.
+      $category_list = get_the_category_list(', ');
+      if ( $category_list ) {
+        echo '<li class="meta-categories"> ' . $category_list . ' </li>';
+      }
+
+      // Get the tags.
+      $tag_list = get_the_tag_list('', ', ');
+      if ( $tag_list ) {
+        echo '<li class="meta-tags"> ' . $tag_list . ' </li>';
+      }
+
+      // Comments link.
+      if ( comments_open() ) {
+        echo '<li><span class="meta-reply">';
+        comments_popup_link( __( 'Leave a comment', 'ftheme' ), __( 'One comment so far', 'alpha' ), __( 'View all % commets', 'alpha' ) );
+        echo '</span></li>';
+      }
+
+      // Edit link.
+      if ( is_user_logged_in() ) {
+        echo '<li>';
+        edit_post_link( __( 'Edit', 'ftheme' ), '<span class="meta-edit">', '</span>' );
+        echo '</li>';
+      }
     }
   }
 }
