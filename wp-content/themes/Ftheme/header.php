54134d1caefc7972112ed5b3ce510c843d6faa68
Bibliaricus <slavikdev95@gmail.com>
2019-05-10 10:32:47 +0300

    Link header, sidebar and footer in index.php

---
+++ wp-content/themes/Ftheme/header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * header.php
+ * 
+ * The header for the theme.
+ */
+?>
+
+<!DOCTYPE html>
+<!-- [if IE 8]> <html <?php language_attributes(); ?> class="ie8"> <![endif] -->
+<!-- [if !IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif] -->
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
+</head>
+<body>
+  
+</body>
+</html>
